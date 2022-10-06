3505711201d3346ddcdd6ecf8b1172cfa8ce7b1c
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2022-10-06 16:47:40 -0700

    mecanum drive - probably will have to adjust later

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
@@ -5,26 +5,33 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class OpMode {
-    @TeleOp(name="void robotics teleop", group="9884")
+    @TeleOp(name="Void Robotics Teleop", group="9884")
     public class TeleOpVoid extends LinearOpMode {
-        //Initializes telemetry
-
+        Hardware robot = new Hardware();
         @Override
         public void runOpMode() {
             //Initializes the hardware map
-            Hardware robot = new Hardware();
             robot.init(hardwareMap, telemetry);
             ElapsedTime runtime = new ElapsedTime();
             //Wait for start (?)
             waitForStart();
             runtime.reset();
-            //while (opModeIsActive()) {
-                //controller
-
-            //    if (gamepad1.dpad_right == true) strafe = -1;
-            //    if (gamepad1.dpad_left == true) strafe = 1;
-            //    if (gamepad1.dpad_up == true) drive = 1;
-             //   if (gamepad1.dpad_down == true) drive = -1;
+            while (opModeIsActive()) {
+                //Mecanum drive - left stick translation, right stick rotation (only x value)
+                double lx = gamepad1.left_stick_x;
+                double ly = -gamepad1.left_stick_y;
+                double rx = gamepad1.right_stick_x;
+                //idk how this works google is a magical place
+                double powerFL=ly+lx+rx, powerFR=ly-lx-rx, powerBL=ly-lx+rx, powerBR=ly+lx-rx;
+                //Normalize values within -1,+1, keep ratios
+                double normalize = Math.max(Math.abs(lx)+Math.abs(ly)+Math.abs(rx),1);
+                powerFL/=normalize;
+                powerFR/=normalize;
+                powerBL/=normalize;
+                powerBR/=normalize;
+                //Power motors
+                robot.powerMotors(powerFL,powerFR,powerBL,powerBR);
+            }
         }
     }
 }
