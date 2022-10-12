4c374f433ff6cfb5840b9ec70b7db722f074e8d2
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2022-10-12 12:55:49 -0700

    added apriltag detection, added claw servos, added slide motor
    added apriltag detection using openftc apriltag + EOCV, NEED TO ADD EOCV TO CONTROL HUB! IMPORANT! deleted unnecessary basic functions (strafe & turn without time controls), added claw servos and viper slide arm motor to hardware initialization, commented all code as much as possible for clarity & future reference.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -1,12 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+/*
+* IMPORTANT
+* ADD EOCV FILE TO ROBOT CONTROL HUB ASAP
+* NEEDED FOR EOCV & APRILTAG DETECTION
+*/
 
 public class Hardware {
     public DcMotor DriveMotorFL = null;
@@ -14,11 +18,21 @@ public class Hardware {
     public DcMotor DriveMotorBL = null;
     public DcMotor DriveMotorBR = null;
 
+    public DcMotor ClawSlide = null;
+
+    public Servo ClawLeft = null;
+    public Servo ClawRight = null;
+
     public void init(HardwareMap hwMap, Telemetry tele) {
         DriveMotorFL = hwMap.dcMotor.get("FL");
         DriveMotorFR = hwMap.dcMotor.get("FR");
         DriveMotorBL = hwMap.dcMotor.get("BL");
         DriveMotorBR = hwMap.dcMotor.get("BR");
+
+        ClawSlide = hwMap.dcMotor.get("CLAWSLIDE");
+
+        ClawLeft = hwMap.servo.get("CLAWLEFT");
+        ClawRight = hwMap.servo.get("CLAWRIGHT");
 
         DriveMotorFL.setDirection(DcMotor.Direction.FORWARD);
         DriveMotorFR.setDirection(DcMotor.Direction.REVERSE);
@@ -26,7 +40,7 @@ public class Hardware {
         DriveMotorBR.setDirection(DcMotor.Direction.REVERSE);
     }
 
-    //basic functions
+    // basic move functions for auto
 
     public void powerMotors(double powerFL, double powerFR, double powerBL, double powerBR) {
         DriveMotorFL.setPower(powerFL);
@@ -41,23 +55,15 @@ public class Hardware {
         powerMotors(0, 0, 0, 0);
     }
 
-    public void turn(double power) {
-        powerMotors(-power, power, -power, power);
-    }
+    // FOR TURN AND STRAFE, -power IS TURN/STRAFE LEFT, +power IS TURN/STRAFE RIGHT
 
-    public void turnTime(double power, int ms) {
-        powerTime(-power, power, -power, power, ms);
-    }
-
-    public void strafe(double power) {
-        powerMotors(power, -power, -power, power);
-    }
-
-    public void strafeTime(double power, int ms) {
+    public void turn(double power, int ms) {
         powerTime(power, -power, power, -power, ms);
     }
 
-    public void sleep(int ms) {
-        try {Thread.sleep(ms);} catch (Exception e) {}
+    public void strafe(double power, int ms) {
+        powerTime(power, -power, power, -power, ms);
     }
+
+    public void sleep(int ms) {try {Thread.sleep(ms);} catch (Exception e) {}}
 }
