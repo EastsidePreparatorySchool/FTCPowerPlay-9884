4748337b4988d6ca31445064726ae0eb55649126
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2022-10-27 16:50:22 -0700

    driveable robot
    ms lewellen told me to commit as this
    but it kinda drives sorta

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Hardware.java
@@ -5,14 +5,11 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-
-/*
-* IMPORTANT
-* ADD EOCV FILE TO CONTROL HUB FIRST FOLDER ASAP
-* NEEDED FOR EOCV & APRILTAG DETECTION
-*/
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 
 public class Hardware {
+    public WebcamName Webcam = null;
+
     public DcMotor DriveMotorFL = null;
     public DcMotor DriveMotorFR = null;
     public DcMotor DriveMotorBL = null;
@@ -23,22 +20,32 @@ public class Hardware {
     public Servo ClawLeft = null;
     public Servo ClawRight = null;
 
+    public final double SPEED_CONSTANT = 0.65;
+    public final double SLOWMODE_CONSTANT = 0.35;
+
+    public Hardware() {}
+
     public void init(HardwareMap hwMap, Telemetry tele) {
+        // Webcam = hwMap.get(WebcamName.class, "Webcam 1");
+
         DriveMotorFL = hwMap.dcMotor.get("FL");
         DriveMotorFR = hwMap.dcMotor.get("FR");
         DriveMotorBL = hwMap.dcMotor.get("BL");
         DriveMotorBR = hwMap.dcMotor.get("BR");
 
-        //ClawSlide = hwMap.dcMotor.get("CLAWSLIDE");
+        // ArmMotor = hwMap.dcMotor.get("ARM");
 
-        //ClawLeft = hwMap.servo.get("CLAWLEFT");
-        //ClawRight = hwMap.servo.get("CLAWRIGHT");
+        // ClawLeft = hwMap.servo.get("CLAWLEFT");
+        // ClawRight = hwMap.servo.get("CLAWRIGHT");
 
         DriveMotorFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         DriveMotorFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         DriveMotorBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         DriveMotorBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        // builders have CTE
+
+        // ArmMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // builders have CTE please colleges do not admit them for engineering
         DriveMotorFL.setDirection(DcMotor.Direction.REVERSE);
         DriveMotorFR.setDirection(DcMotor.Direction.REVERSE);
         DriveMotorBL.setDirection(DcMotor.Direction.REVERSE);
@@ -72,5 +79,5 @@ public class Hardware {
 
     public void threadsleep(int ms) {try {Thread.sleep(ms);} catch (Exception e) {}}
 
-    // SLIDE AND CLAW FUNCTIONS HERE
+    // ARM AND CLAW FUNCTIONS HERE
 }
