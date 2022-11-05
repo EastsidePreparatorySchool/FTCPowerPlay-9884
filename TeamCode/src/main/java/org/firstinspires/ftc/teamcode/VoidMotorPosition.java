15181f980fd2ce3c5df81c5c727420db9f7b57fd
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2022-11-05 13:09:41 -0700

    literally everything
    everything we need for functioning - arm code, preset encoders, claw functioning, basically full teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoidMotorPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoidMotorPosition.java
@@ -9,41 +9,75 @@ public class VoidMotorPosition extends LinearOpMode {
     // Initialize hardware
     Hardware robot = new Hardware();
     // in ticks
-    int clawPos = 0;
+    int armPos = 0;
+    double clawPos = robot.CLAW_OPEN_POSITION;
+    boolean armBtnReleased = true;
+    boolean clawBtnReleased = true;
 
     @Override
     public void runOpMode() {
         // Initializes the hardware map
         robot.init(hardwareMap, telemetry);
-        telemetry.addData("lmao", "lmao");
         telemetry.update();
         waitForStart();
+        robot.setClawRot(robot.CLAW_OPEN_POSITION);
         while (opModeIsActive()) {
-            robot.setArmPos(clawPos);
 
-            if(gamepad1.dpad_up) {
-                clawPos+=30;
+            if(gamepad1.right_bumper && armBtnReleased) {
+                armPos += robot.ARM_INCREMENT_ENCODER_CONSTANT;
+                if(armPos > robot.ARM_NEVER_EXCEED) {
+                    armPos = robot.ARM_NEVER_EXCEED;
+                }
+                robot.ArmMotor.setTargetPosition(armPos);
+                armBtnReleased=false;
             }
-            else if(gamepad1.dpad_down) {
-                clawPos-=15;
+            else if(gamepad1.left_bumper && armBtnReleased) {
+                armPos -= robot.ARM_INCREMENT_ENCODER_CONSTANT/2;
+                if(armPos < 0) {
+                    armPos = 0;
+                }
+                robot.ArmMotor.setTargetPosition(armPos);
+                armBtnReleased=false;
+            }
+            if(!gamepad1.left_bumper && !gamepad1.right_bumper) {
+                armBtnReleased = true;
             }
 
-            if(gamepad1.a) {
-                telemetry.addData("pos: ", robot.ArmMotor.getCurrentPosition());
-            }
+
             /*
-            else if(gamepad1.left_bumper) {
-                robot.setClawPos(clawPos-0.02);
-                clawPos-=0.02;
+            if(gamepad1.left_bumper && clawBtnReleased) {
+                if(clawPos-0.01<0) {
+                    clawPos+=0.01;
+                }
+                robot.setClawRot(clawPos-0.01);
+                clawPos-=0.01;
+                clawBtnReleased=false;
             }
-            else if(gamepad1.right_bumper) {
-                robot.setClawPos(clawPos+0.02);
-                clawPos+=0.02;
+            else if(gamepad1.right_bumper && clawBtnReleased) {
+                robot.setClawRot(clawPos+0.01);
+                clawPos+=0.01;
+                clawBtnReleased=false;
             }
-            else if(gamepad1.b) {
-                telemetry.addData("pos: ", robot.ClawLeft.getPosition());
+
+            if(!gamepad1.left_bumper && !gamepad1.right_bumper) {
+                clawBtnReleased = true;
             }
             */
+
+            if (gamepad1.a) {
+                robot.setClawRot(robot.CLAW_OPEN_POSITION);
+            }
+            if (gamepad1.b) {
+                robot.setClawRot(robot.CLAW_CLOSED_POSITION);
+            }
+
+
+
+            telemetry.addData("Arm Current Position", robot.ArmMotor.getCurrentPosition());
+            telemetry.addData("Arm Target", armPos);
+            telemetry.addData("Arm Internal Target", robot.ArmMotor.getTargetPosition());
+
+            telemetry.update();
         }
     }
 }
